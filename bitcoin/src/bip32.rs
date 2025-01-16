2021778939210ee8a9c28e9cde69820d3c9dc390
Mehmet Efe Akça <efeakca@gmail.com>
2025-01-16 23:59:46 +0300

    Update secp256k1 to 0.30.0 (v0.32.5 branch in bitcoin)
    Migrated `from_slice` usage to `from_byte_array` with proper error
    forwarding when needed.
    Converted to Message::as_ref<[u8]> for signing due to API change of
    receiving arbitrary byte arrays in lieu of Message hashes only.
    Updated lockfiles.
    
    `from_slice` has been deprecated by #3102 due to better support for arrays in Rust.
    BIP340 supports arbitrary byte arrays as discussed on rust-bitcoin/rust-secp256k1#706
    
    Closes #3482

--- bitcoin/src/bip32.rs
+++ bitcoin/src/bip32.rs
@@ -574,7 +574,11 @@ impl Xpriv {
             depth: 0,
             parent_fingerprint: Default::default(),
             child_number: ChildNumber::from_normal_idx(0)?,
-            private_key: secp256k1::SecretKey::from_slice(&hmac_result[..32])?,
+            private_key: secp256k1::SecretKey::from_byte_array(
+                &hmac_result.as_byte_array()[..32]
+                    .try_into()
+                    .expect("Internal HMAC hash is broken: shorter than 32 bytes"),
+            )?,
             chain_code: ChainCode::from_hmac(hmac_result),
         })
     }
@@ -629,8 +633,12 @@ impl Xpriv {
 
         hmac_engine.input(&u32::from(i).to_be_bytes());
         let hmac_result: Hmac<sha512::Hash> = Hmac::from_engine(hmac_engine);
-        let sk = secp256k1::SecretKey::from_slice(&hmac_result[..32])
-            .expect("statistically impossible to hit");
+        let sk = secp256k1::SecretKey::from_byte_array(
+            &hmac_result.as_byte_array()[..32]
+                .try_into()
+                .expect("HMAC internal hash is broken: shorter than 32 bytes"),
+        )
+        .expect("statistically impossible to hit");
         let tweaked =
             sk.add_tweak(&self.private_key.into()).expect("statistically impossible to hit");
 
@@ -669,7 +677,9 @@ impl Xpriv {
             chain_code: data[13..45]
                 .try_into()
                 .expect("45 - 13 == 32, which is the ChainCode length"),
-            private_key: secp256k1::SecretKey::from_slice(&data[46..78])?,
+            private_key: secp256k1::SecretKey::from_byte_array(
+                data[46..78].try_into().expect("78 - 46 == 32, which is the SecretKey length"),
+            )?,
         })
     }
 
@@ -750,7 +760,11 @@ impl Xpub {
 
                 let hmac_result: Hmac<sha512::Hash> = Hmac::from_engine(hmac_engine);
 
-                let private_key = secp256k1::SecretKey::from_slice(&hmac_result[..32])?;
+                let private_key = secp256k1::SecretKey::from_byte_array(
+                    &hmac_result.as_byte_array()[..32]
+                        .try_into()
+                        .expect("HMAC internal hash is broken: shorter than 32 bytes"),
+                )?;
                 let chain_code = ChainCode::from_hmac(hmac_result);
                 Ok((private_key, chain_code))
             }
--- bitcoin/src/crypto/key.rs
+++ bitcoin/src/crypto/key.rs
@@ -443,7 +443,12 @@ impl PrivateKey {
         data: &[u8],
         network: impl Into<NetworkKind>,
     ) -> Result<PrivateKey, secp256k1::Error> {
-        Ok(PrivateKey::new(secp256k1::SecretKey::from_slice(data)?, network))
+        Ok(PrivateKey::new(
+            secp256k1::SecretKey::from_byte_array(
+                data.try_into().map_err(|_| secp256k1::Error::InvalidSecretKey)?,
+            )?,
+            network,
+        ))
     }
 
     /// Format the private key to WIF format.
@@ -493,7 +498,9 @@ impl PrivateKey {
         Ok(PrivateKey {
             compressed,
             network,
-            inner: secp256k1::SecretKey::from_slice(&data[1..33])?,
+            inner: secp256k1::SecretKey::from_byte_array(
+                data[1..33].try_into().expect("slice len checked == 32"),
+            )?,
         })
     }
 }
